e85d066a35f9bc1fc6b933e89255ff1ff3d334cd
pratham2112006-coder <pratham2112006@gmail.com>
2025-12-29 14:34:56 +0530

    Create validation.js
    input valdation

---
+++ middlewares/validation.js
@@ -0,0 +1,41 @@
+const { body, validationResult } = require("express-validator");
+
+const signupValidation = [
+  body("username")
+    .notEmpty().withMessage("Username is required")
+    .isLength({ min: 3 }).withMessage("Username must be at least 3 characters"),
+
+  body("email")
+    .notEmpty().withMessage("Email is required")
+    .isEmail().withMessage("Invalid email format"),
+
+  body("firstName")
+    .notEmpty().withMessage("First name is required")
+    .isLength({ min: 3 }).withMessage("First name must be at least 3 characters"),
+
+  body("lastName")
+    .notEmpty().withMessage("Last name is required")
+    .isLength({ min: 3 }).withMessage("Last name must be at least 3 characters"),
+
+  body("password")
+    .notEmpty().withMessage("Password is required")
+    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters")
+    .matches(/[A-Z]/).withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/).withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/).withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one special character"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false,
+        errors: errors.array()
+      });
+    }
+    next();
+  }
+];
+
+module.exports = { signupValidation };
