f635d7014d4dcef5d230e06a4912a045a681b243
Deepak Aggarwal <deepakagg1206@gmail.com>
2025-12-30 14:42:44 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors")
 const { userRouter } = require('./routes/user.routes');
 const { AQIRouter } = require('./routes/aqi.routes');
 const mongoose = require('mongoose');
+const { any } = require('zod');
 
 const app = express();
+app.use(cors({
+    origin: ["http://localhost:5173"]
+}))
 app.use(express.json())
 
 app.use("/api/user",userRouter);
